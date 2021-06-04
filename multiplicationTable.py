98e68f8880ee85ed50b646e9ed116390033d4e1f
khushi <chhayak23@greenhill.org>
2021-06-03 19:55:17 -0500

    timestable commit (correct file)

---
+++ multiplicationTable.py
@@ -0,0 +1,45 @@
+for k in range (1,11):
+    print (k, end= '   ')
+print ()
+for i in range (2,11):
+    print (i)
+base = 1
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 2
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 3
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 4
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 5
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 6
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 7
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 8
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 9
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
+base = 10
+for a in range (1,11):
+    print (base * a, end= '  ')
+print ()
