2718fba07f7cb1074f690f3be7303b32f860f8d2
khushi <chhayak23@greenhill.org>
2021-06-08 10:36:39 -0500

    second commit in class game menu

--- InClassGameMenu.py
+++ InClassGameMenu.py
@@ -3,6 +3,7 @@
 #Modifying Quentin's code
 #if conditional
 #funciton pieces of code that we can reuse
+#to declare a function, must define function (keyword def), then name
 l1="**************************"
 l2="*       My game          *"
 l3="*                        *"
@@ -11,31 +12,7 @@ l5="*    2 - Level2          *"
 l6="*    3 - Scores          *"
 l7="*    4 - Exit Game       *"
 l8="**************************"
-print(l1)
-print(l2)
-print(l3)
-print(l4)
-print(l5)
-print(l6)
-print(l7)
-print(l8)
-print('please enter your selection 1-4')
-x=int(input())
-while x is not '4': #loop is conditioned to an event
-    if x is '1':          #if statement are selection or branching(else)
-        print("Level 1 Chosen")
-    if x is '2':
-        print("Level 2 Chosen")
-    if x is '3':
-        print("Scores Chosen")
-        print(l1)
-        print("*        Scores          *")
-        print(l3)
-        print("*    1 - ???- 999        *")
-        print("*    2 - ???- 876        *")
-        print("*    3 - ???- 745        *")
-        print(l3)
-        print(l8)
+def menu():
     print(l1)
     print(l2)
     print(l3)
@@ -45,6 +22,30 @@ while x is not '4': #loop is conditioned to an event
     print(l7)
     print(l8)
     print('please enter your selection 1-4')
-    x=int(input())
-if x is '4':
-    print("Goodbye! Thank you for playing!")
+    inputNumber = input()
+    x=int(inputNumber)
+    return x
+def score():
+    print("Scores Chosen")
+    print(l1)
+    print("*        Scores          *")
+    print(l3)
+    print("*    1 - ???- 999        *")
+    print("*    2 - ???- 876        *")
+    print("*    3 - ???- 745        *")
+    print(l3)
+    print(l8)
+x=1 #global variable
+
+
+while (x !=4): #loop is conditioned to an event\
+    x=menu()         #this is a funciton call
+    if (x==1):          #if statement are selection or branching(else)
+        print("Level 1 Chosen")
+
+    if (x==2):
+        print("Level 2 Chosen")
+
+    if (x==3):
+        score()
+print("Goodbye! Thank you for playing!")
