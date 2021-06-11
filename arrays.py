db06f99dda86afdea55674066a877a05de1ce716
khushi <chhayak23@greenhill.org>
2021-06-11 09:28:37 -0500

    commit array practice with lists and tuples

--- arrays.py
+++ arrays.py
@@ -2,6 +2,10 @@
 #6/10/21
 #we are going to learn about lists and tuples
 #learn their funcitons and looping with list
+
+# HOW to use a module or library
+import random 
+
 MyFruit=['apples', 'berries', 'mangos', 'banana']
 print(MyFruit)
 for fruit in MyFruit:
@@ -10,9 +14,14 @@ for fruit in MyFruit:
 for fruit in MyFruit: #for the length of your array
     print(fruit, end= ', ')
 counter=len(MyFruit) #lenght of list is one more than last index
-
+index=random.randint(0,counter-1)
+print('your lucky fruit is', MyFruit[index])
 for x in range(0,counter-1):
     print(MyFruit[x], end= ', ')
+
+#random method choice()
+word=random.choice(MyFruit)
+print('your random fruit is', word)
 
 print(MyFruit[counter-1]) #print last term
 
@@ -25,4 +34,14 @@ MyFruit.insert(0, 'kiwi')
 MyFruit.insert(2, 'papaya')
 print(MyFruit)
 
-
+#tuple is a list that is unchangeable
+fruity=('apple', 'pears', 'banana')
+print(fruity)
+temp=list(fruity) #convert to list to modify
+print(temp)
+temp.insert(3, 'mango')
+fruity=tuple(temp)
+print('tuple', fruity)
+print('list', temp)
+for element in fruity:
+    print(element)
